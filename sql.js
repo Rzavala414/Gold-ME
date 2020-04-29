1295212034305df95a34f6411a080d5b0dd0f377
Roger Zavala <rogeliozavala15@gmail.com>
2020-04-29 16:26:17 -0700

    created query for most winter wins

--- sql.js
+++ sql.js
@@ -62,7 +62,13 @@ won the most winter medals, along with the number of medals aliased to 'count'.
 */
 
 const mostWinterWins = country => {
-  return;
+  return `SELECT year, COUNT(*) AS count
+  FROM GoldMedal
+  WHERE country = ${country}
+    AND season = winter
+    GROUP BY year
+    ORDER BY COUNT(*) DESC
+    LIMIT 1; `;
 };
 
 /*
